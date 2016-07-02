ab189302adcb867c3a607a33211a714967194290
ryotamac <tennis.suta6@gmail.com>
2016-07-02 22:27:20 +0900

    modelの変更

--- src/client/domain/model/metrics.ts
+++ src/client/domain/model/metrics.ts
@@ -4,47 +4,50 @@ import Computer,{createComputer} from './computer';
 import * as _ from 'lodash';
 export default class Metrics {
   id: number;
-  rid: string;
-  requests: Array<Request>;
-  processes: Array<Process>;
-  computers: Array<Computer>;
-  constructor(param = { id: 0, rid: "", requests: [], processes: [], computers: [] }) {
-    this.requests = param.requests;
-    this.processes = param.processes;
-    this.computers = param.computers;
+  rid: number;
+  request: Request;
+  process: Process;
+  computer: Computer;
+  constructor(param = { id: 0, rid: 0, request: new Request, process: new Process, computer: new Computer }) {
+    this.request = param.request;
+    this.process = param.process;
+    this.computer = param.computer;
     this.id = param.id;
     this.rid = param.rid;
   }
   setTimeFormat(format) {
-    const processes = this.processes.map(elem => elem.setTimeFormat(format));
-    const computers = this.computers.map(elem => elem.setTimeFormat(format));
+    const process = this.process.setTimeFormat(format);
+    const computer = this.computer.setTimeFormat(format);
     return new Metrics(Object.assign({}, this, {
-      processes,
-      computers
+      process,
+      computer
     }))
   }
-  compact(size) {
-    if (this.processes.length / size < 2) return this;
-    const processes = this._compact<Process>('processes', size, ["pid", "mid", "relativeTime"], createProcess);
-    const computers = this._compact<Computer>('computers', size, ["mid", "relativeTime"], createComputer);
-    const requests = this._compact<Request>('requests', size, ["mid", "statusCode"], createRequest);
-    return new Metrics(Object.assign({}, this, {processes}, {computers}, {requests}));
-  }
-  private _compact<T>(propname, size, pass: Array<string> = [], fun = (a) => a): Array<T> {
-    const futureSize = this[propname].length / size;
-    const chunkArr = _.chunk(this[propname], futureSize);
-    return chunkArr.map(arr => objectAverage(arr, pass)).map(ave => fun(ave));
-  }
+  // compact(size) {
+  //   if (this.processes.length / size < 2) return this;
+  //   const processes = this._compact<Process>('processes', size, ["pid", "mid", "relativeTime"], createProcess);
+  //   const computers = this._compact<Computer>('computers', size, ["mid", "relativeTime"], createComputer);
+  //   const requests = this._compact<Request>('requests', size, ["mid", "statusCode"], createRequest);
+  //   return new Metrics(Object.assign({}, this, {processes}, {computers}, {requests}));
+  // }
+  // private _compact<T>(propname, size, pass: Array<string> = [], fun = (a) => a): Array<T> {
+  //   const futureSize = this[propname].length / size;
+  //   const chunkArr = _.chunk(this[propname], futureSize);
+  //   return chunkArr.map(arr => objectAverage(arr, pass)).map(ave => fun(ave));
+  // }
 }
 
 export function createMetrics(param: {
-  requests: Array<Request>;
-  processes: Array<Process>;
-  computers: Array<Computer>;
+  request: Request;
+  process: Process;
+  computer: Computer;
   id: number;
-  rid: string;
+  rid: number;
 }) {
-  return new Metrics(param);
+  const request = createRequest(param.request);
+  const process = createProcess(param.process);
+  const computer = createComputer(param.computer);
+  return new Metrics({ request, process, computer, id: param.id, rid: param.rid });
 }
 
 
--- src/client/domain/model/running.ts
+++ src/client/domain/model/running.ts
@@ -1,11 +1,11 @@
 import Metrics from './metrics';
 export default class Running {
   name: string;
-  id: string;
+  id: number;
   duration: number;
   arrivalRate: number;
   metricses: Array<Metrics>;
-  constructor(param = { name: "", id: "", duration: 0, arrivalRate: 0 , metricses: []}) {
+  constructor(param = { name: "", id: 0, duration: 0, arrivalRate: 0 , metricses: []}) {
     this.name = param.name;
     this.id = param.id;
     this.duration = param.duration;
@@ -13,7 +13,7 @@ export default class Running {
     this.metricses = param.metricses;
   }
 }
-export function createRunning(param: { name: string, id: string, duration: number, arrivalRate: number }, metricses: Array<Metrics>) {
+export function createRunning(param: { name: string, id: number, duration: number, arrivalRate: number }, metricses: Array<Metrics>) {
   const { name, id, duration, arrivalRate } = param;
   return new Running({ metricses, id, duration, arrivalRate, name });
 }
